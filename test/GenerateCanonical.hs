a208871794d8a6395a12d55212bda6b405e2cb80
Isaac Van Doren <69181572+isaacvando@users.noreply.github.com>
2022-11-05 19:14:28 -0500

    more progress on setting up canonical tests

--- test/GenerateCanonical.hs
+++ test/GenerateCanonical.hs
@@ -3,6 +3,7 @@ import Cook hiding (step, metadata, ingredient, cookware, timer) -- (Metadata, S
 import Text.Megaparsec
 import Text.Megaparsec.Char
 import Data.Void
+import Data.List
 
 -- type Parser = Parsec Void String
         -- Name Input Metadatas Steps
@@ -11,28 +12,36 @@ data Test = Test String String [Metadata] [Step] deriving Show
 generate :: IO ()
 generate = do
     file <- readFile "test/canonical.yaml"
-    print $ parse test "" file
+    case (parse testFile "" file) of
+        Left x -> putStrLn $ errorBundlePretty x
+        Right x -> print x
     return ()
+
+testFile :: Parser [Test]
+testFile = do
+    string "version: 5" *> space *> string "tests:" *> space
+    ts <- some $ test <* space
+    return ts
 
 test :: Parser Test
 test = do
     string "test"
     name <- some $ noneOf ":"
     char ':' *> space
-    string "source: |\n" *> space
-    input <- some $ noneOf "\n"
+    string "source: |\n"
+    input <- some $ string "      " *> (some $ noneOf "\n") <* newline
     space *> string "result:"
     space *> string "steps:" *> space
-    resultSteps <- some step
+    resultSteps <- some $ step <* space
     resultMetadata <- space *> string "metadata:" *> space *> (string "{}" *> return []) <|> some metadata
-    return $ Test name input resultMetadata resultSteps
+    return $ Test name (intercalate "\n" input) resultMetadata resultSteps
 
 step :: Parser Step
 step = do
     (string "[]" *> return []) 
     <|> (do 
             char '-' *> space
-            contents <- some text
+            contents <- some (try text <|> ingredient <?> "step part")
             return contents)
 
 metadata :: Parser Metadata
@@ -43,10 +52,35 @@ metadata = do
     value <- some $ noneOf "\n"
     return (key, value)
 
-
 text :: Parser Content
 text = do 
     string "- type: text" *> space *> string "value: \""
     content <- some $ noneOf "\""
     char '\"'
     return $ Text content
+
+ingredient :: Parser Content
+ingredient = do
+    string "- type: ingredient" *> space *> string "name: "
+    name <- char '\"' *> (some $ noneOf "\"") <* char '\"' <* space <* string "quantity: "
+    amount <- (many $ noneOf "\n") <* space <* string "units: "
+    units <- char '\"' *> (many $ noneOf "\"") <* char '\"' <* space
+    return $ Ingredient name amount units
+
+cookware :: Parser Content
+cookware = do
+    string "- type: cookware" *> space *> string "name: \""
+    name <- many $ noneOf "\""
+    char '\"' *> space *> string "quantity: "
+    amount <- many $ noneOf "\n"
+    return $ Cookware name amount
+
+timer :: Parser Content
+timer = do
+    string "- type: timer" *> space *> string "quantity: "
+    amount <- many $ noneOf "\n"
+    space *> string "units: \""
+    units <- many $ noneOf "\""
+    char '\"' *> space *> string "name: \""
+    name <- many $ noneOf "\""
+    return $ Timer name amount units
